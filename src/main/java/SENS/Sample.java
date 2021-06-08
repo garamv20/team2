a51d9f39af2ff35df9a1f8984b0ad800725fdf01
playnauts <82129220+playnauts@users.noreply.github.com>
2021-06-08 10:41:37 +0900

    샘플 설명 수정

--- src/main/java/SENS/Sample.java
+++ src/main/java/SENS/Sample.java
@@ -6,6 +6,7 @@ public class Sample {
 		Send.SMS("01048516339", "테스트 메시지 전송", "ncp:sms:kr:262195421504:message_sender", "1zB1FNZZofZBbLlwgesB", "wRREJhsLJRf46hyM28laTvoqp5yIpnndEuRYeM9A");
 		//참고사항
 		//SMS는 단문, LMS는 장문
+		//수신자 번호, 메시지 내용, 키, 키, 키
 		//발신번호는 01048516339로 고정, 변경 불가
 	}
 
